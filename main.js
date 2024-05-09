6f2e25950b8412dcde9d0fe3098fd47ec4f99b9d
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2024-05-08 23:41:51 -0400

    Rearrange panels

--- main.js
+++ main.js
@@ -14,16 +14,16 @@ function ui(divID) {
 <div id="header" class="mx-auto max-w-full px-2 py-0 bg-black h-1/10 relative z-10"> <div class="flex items-center justify-between">
         <div class="flex justify-start gap-2 pl-24">
             <div class="flex items-center">
-                <img src="${vectorGazerLogo}" class="h-36 w-36 -mb-10 logo vanilla" alt="vector gazer logo" />
+                <img src="${vectorGazerLogo}" class="h-32 w-32 -mb-10 logo vanilla" alt="vector gazer logo" />
             </div>
             <div class="min-w-0 flex-1 flex items-center">
-                <h1 class="text-4xl font-bold leading-7 text-white">Vector Gazer</h1>
+                <h1 class="text-3xl font-bold leading-7 text-white">Vector Gazer</h1>
             </div>
         </div>
         
         <div class="flex md:mt-0 md:ml-4 shrink-0 pr-24">
             <a title="Source code" href="https://github.com/jeyabbalas/vector-gazer">
-                <img src="${githubLogo}" class="h-16 w-16 fill-current" alt="github logo" />
+                <img src="${githubLogo}" class="h-14 w-14 fill-current" alt="github logo" />
             </a>
         </div>
     </div>
@@ -33,15 +33,15 @@ function ui(divID) {
 
 <div class="flex mx-auto max-w-full h-9/10" style="height: calc(100vh - 75px);">
     <!-- Left panel: data, embedding API, embedding model, and projection method -->
-    <div class="w-3/12 bg-black p-4 mt-8 flex flex-col h-full">
+    <div class="w-3/12 bg-black p-4 mt-5 flex flex-col h-full">
         <!-- Data upload -->
         <div id="data-panel" class="relative py-2" title="The file should be a JSON array of objects. Each object must contain the 'text' key with a string value representing the text for embedding. Optionally, include: 1) 'embedding': An array of floats for pre-computed embeddings, and 2) 'label': A brief text description for the scatter plot entry.">
-            <h2 class="absolute transform left-4 -translate-y-1/2 bg-black px-2 text-lg font-bold text-white whitespace-nowrap">Data</h2>
+            <h2 class="absolute transform left-4 -translate-y-1/2 bg-black px-2 font-bold text-white whitespace-nowrap">Data</h2>
             <div class="rounded-md border border-white mb-4 p-2 flex flex-col items-center pt-6">
                 <!-- File upload -->
                 <div class="relative rounded-md rounded-b-none px-1.5 pb-1.5 pt-1.5 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
-                    <label for="data-upload" class="block font-medium text-white">File upload</label>
-                    <input type="file" id="data-upload" name="data-upload" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" accept=".json" required>
+                    <label for="data-upload" class="block font-medium text-sm text-white">File upload</label>
+                    <input type="file" id="data-upload" name="data-upload" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" accept=".json" required>
                 </div>
                 <!-- or -->
                 <div class="relative w-full my-1">
@@ -54,14 +54,14 @@ function ui(divID) {
                 </div>
                 <!-- URL input -->
                 <div class="relative rounded-md rounded-t-none px-1.5 pb-1.5 pt-1.5 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
-                    <label for="data-url" class="block font-medium text-white">URL</label>
-                    <input type="url" id="data-url" name="data-url" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" placeholder="Enter URL" required>
+                    <label for="data-url" class="block font-medium text-sm text-white">URL</label>
+                    <input type="url" id="data-url" name="data-url" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" placeholder="Enter URL" required>
                 </div>
                 <!-- reset and submit buttons -->
-                <div class="py-2 mt-2">
+                <div class="py-1 mt-1">
                     <div class="flex justify-center gap-2">
-                        <button id="reset-data" class="rounded-md border border-white bg-red-800 text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-white">Reset</button>
-                        <button id="submit-data" class="rounded-md border border-white bg-gray-800 text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-1 focus:ring-white">Submit</button>
+                        <button id="reset-data" class="rounded-md border border-white bg-red-800 text-sm text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-white">Reset</button>
+                        <button id="submit-data" class="rounded-md border border-white bg-gray-800 text-sm text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-1 focus:ring-white">Submit</button>
                     </div>
                 </div>
             </div>
@@ -69,25 +69,25 @@ function ui(divID) {
 
         <!-- Embeddings API configuration -->
         <div id="config-panel" class="relative py-2">
-            <h2 class="absolute left-4 transform -translate-y-1/2 bg-black px-2 text-lg font-bold text-white whitespace-nowrap">Embeddings API</h2>
+            <h2 class="absolute left-4 transform -translate-y-1/2 bg-black px-2 font-bold text-white whitespace-nowrap">Embeddings API</h2>
         
             <div class="rounded-md border border-white mb-4 p-2 flex flex-col items-center pt-6">
                 <div class="relative rounded-md rounded-b-none px-1.5 pb-1.5 pt-1.5 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
-                    <label for="base-url" class="block font-medium text-white">Base URL</label>
-                    <input type="url" id="base-url" name="base-url" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" placeholder="https://api.openai.com/v1" value="https://api.openai.com/v1" required>
+                    <label for="base-url" class="block font-medium text-sm text-white">Base URL</label>
+                    <input type="url" id="base-url" name="base-url" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" placeholder="https://api.openai.com/v1" value="https://api.openai.com/v1" required>
                 </div>
         
                 <div class="relative rounded-md rounded-t-none px-1.5 pb-1.5 pt-1.5 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
-                    <label for="api-key" class="block font-medium text-white">API key</label>
-                    <input type="password" id="api-key" name="api-key" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" required>
+                    <label for="api-key" class="block font-medium text-sm text-white">API key</label>
+                    <input type="password" id="api-key" name="api-key" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" required>
                 </div>
         
                 <div id="api-key-message-container"></div>
         
-                <div id="submit-api-key-buttons" class="py-2 mt-2">
+                <div id="submit-api-key-buttons" class="py-1 mt-1">
                     <div class="flex justify-center gap-2">
-                        <button id="forget-api-key" class="rounded-md border border-white bg-red-800 text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-white">Forget API key</button>
-                        <button id="submit-api-key" class="rounded-md border border-white bg-gray-800 text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-1 focus:ring-white">Submit</button>
+                        <button id="forget-api-key" class="rounded-md border border-white bg-red-800 text-sm text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-white">Forget API key</button>
+                        <button id="submit-api-key" class="rounded-md border border-white bg-gray-800 text-sm text-white py-0.5 px-1.5 font-medium shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-1 focus:ring-white">Submit</button>
                     </div>
                 </div>
             </div>
@@ -95,28 +95,22 @@ function ui(divID) {
         
         <!-- Embeddings model selection -->
         <div id="model-panel" class="relative py-2"> 
-            <h2 class="absolute left-4 transform -translate-y-1/2 bg-black px-2 text-lg font-bold text-white whitespace-nowrap">Embedding model</h2>
+            <h2 class="absolute left-4 transform -translate-y-1/2 bg-black px-2 font-bold text-white whitespace-nowrap">Embedding model</h2>
+            
             <div class="rounded-md border border-white mb-4 p-2 flex flex-col items-center pt-6"> 
-                <div class="relative rounded-md px-1.5 pb-1.5 pt-1.5 mb-4 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
-                    <label for="model" class="block font-medium text-white">Model</label>
-                    <select id="model" name="model" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" required>
+                <div class="relative rounded-md rounded-b-none px-1.5 pb-1.5 pt-1.5 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
+                    <label for="model" class="block font-medium text-sm text-white">Model</label>
+                    <select id="model" name="model" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" required>
                         <option value="" disabled selected>Set URL/API to view models</option>
                     </select>
                 </div>
+                <!-- Slider titled "Dimension". By default, it is disabled and takes range 0 to 0. It also displays the current value to the right of the slider. -->
+                <div class="relative rounded-md rounded-t-none px-1.5 pb-1.5 pt-1.5 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
+                    <label for="dimension" class="block font-medium text-sm text-white">Dimension</label>
+                    <output for="dimension" class="block text-sm text-white text-center">0</output>
+                    <input type="range" id="dimension" name="dimension" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" min="0" max="0" value="0" disabled>
+                </div>
             </div>
-        </div>
-
-        <!-- Projection method selection -->
-        <h2 class="text-xl font-bold text-white mb-4">Projection method</h2>
-        <div id="projection-panel" class="rounded-md border border-white p-2 flex-grow">
-            <ul class="flex border-b border-gray-300 text-white mb-2">
-                <li class="-mb-px mr-1">
-                    <a class="rounded-t-md bg-gray-800 inline-block py-2 px-4 text-white" href="#pca">PCA</a>
-                </li>
-                <li class="mr-1">
-                    <a class="rounded-t-md bg-gray-900 inline-block py-2 px-4 text-white" href="#umap">UMAP</a>
-                </li>
-            </ul>
         </div>
     </div>
 
@@ -130,12 +124,20 @@ function ui(divID) {
     <!-- Vertical line between scatter plot and right Panel -->
     <div class="w-px bg-white h-screen"></div>
 
-    <!-- Right panel: query -->
-    <div class="w-3/12 bg-black p-4 mt-4 flex flex-col h-full">
-        <h2 class="text-xl font-bold text-white mb-4">Query</h2>
-        <div id="query-panel" class="rounded-md border border-white p-2 flex-grow">
-            <!-- Placeholder content for Query section -->
-            <p class="text-white">Query section content</p>
+    <!-- Right panel: projection method and query -->
+    <div class="w-3/12 bg-black p-4 mt-5 flex flex-col h-full">
+        <!-- Projection method selection -->
+        <div id="projection-panel" class="relative py-2"> 
+            <h2 class="absolute left-4 transform -translate-y-1/2 bg-black px-2 font-bold text-white whitespace-nowrap">Projection method</h2>
+            <div class="rounded-md border border-white mb-4 p-2 flex flex-col items-center pt-6"> 
+            </div>
+        </div>
+        
+        <!-- Query -->
+        <div id="query-panel" class="relative py-2"> 
+            <h2 class="absolute left-4 transform -translate-y-1/2 bg-black px-2 font-bold text-white whitespace-nowrap">Query</h2>
+            <div class="rounded-md border border-white mb-4 p-2 flex flex-col items-center pt-6"> 
+            </div>
         </div>
     </div>
 </div>
@@ -288,7 +290,7 @@ submitApiKeyBtn.addEventListener('click', async () => {
 
         // Display success message
         const successMessage = document.createElement('div');
-        successMessage.classList.add('bg-green-50', 'border', 'border-green-300', 'text-green-800', 'px-2', 'py-1', 'rounded', 'relative', 'text-sm', 'mt-4');
+        successMessage.classList.add('bg-green-50', 'border', 'border-green-300', 'text-green-800', 'px-2', 'py-1', 'rounded', 'relative', 'text-sm', 'mt-2');
         successMessage.innerHTML = '<strong class="font-bold">Success!</strong> API key is valid.';
         apiKeyMessageContainer.appendChild(successMessage);
     } else {  // API key is invalid
@@ -297,7 +299,7 @@ submitApiKeyBtn.addEventListener('click', async () => {
 
         // Display error message
         const errorMessage = document.createElement('div');
-        errorMessage.classList.add('bg-red-50', 'border', 'border-red-300', 'text-red-800', 'px-2', 'py-1', 'rounded', 'relative', 'text-sm', 'mt-4');
+        errorMessage.classList.add('bg-red-50', 'border', 'border-red-300', 'text-red-800', 'px-2', 'py-1', 'rounded', 'relative', 'text-sm', 'mt-2');
         errorMessage.innerHTML = '<strong class="font-bold">Error:</strong> Invalid API key.';
         apiKeyMessageContainer.appendChild(errorMessage);
     }
