9ad12c9bf23f8d9e42b07abfaedb4693e1fa907a
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2024-05-09 02:15:54 -0400

    Display loading screen while embedding

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ import { manageOpenAIApiKey, Embedding } from "./embedding.js";
 function ui(divID) {
     let divUI = divID ? document.getElementById(divID) : document.createElement('div');
 
-    // <h1 class="text-3xl font-bold underline"> Hello world! </h1>
     divUI.innerHTML = `
 <!-- Header -->
 <div id="header" class="mx-auto max-w-full px-2 py-0 bg-black h-1/10 relative z-10"> <div class="flex items-center justify-between">
@@ -43,6 +42,7 @@ function ui(divID) {
                     <label for="data-upload" class="block font-medium text-sm text-white">File upload</label>
                     <input type="file" id="data-upload" name="data-upload" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" accept=".json" required>
                 </div>
+                
                 <!-- or -->
                 <div class="relative w-full my-1">
                     <div class="absolute inset-0 flex items-center">
@@ -52,11 +52,13 @@ function ui(divID) {
                         <span class="bg-black px-2 text-gray-400">or</span>
                     </div>
                 </div>
+                
                 <!-- URL input -->
                 <div class="relative rounded-md rounded-t-none px-1.5 pb-1.5 pt-1.5 w-full ring-1 ring-inset ring-gray-400 focus-within:z-10 focus-within:ring-2 focus-within:ring-white">
                     <label for="data-url" class="block font-medium text-sm text-white">URL</label>
                     <input type="url" id="data-url" name="data-url" class="block rounded-sm w-full border-0 p-1 mb-1 bg-gray-800 text-sm text-white placeholder:text-gray-400 focus:outline-none focus:ring-0 focus:border-0" placeholder="Enter URL" required>
                 </div>
+                
                 <!-- reset and submit buttons -->
                 <div class="py-1 mt-1">
                     <div class="flex justify-center gap-2">
@@ -334,10 +336,23 @@ dimensionSlider.addEventListener('input', () => {
 
 
 submitModelBtn.addEventListener('click', () => {
+    const originalHTML = submitModelBtn.innerHTML;
+    submitModelBtn.innerHTML = `
+                <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-2 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                </svg>
+                Embedding data...
+            `;
+    submitModelBtn.disabled = true;
     const model = modelsListDropdown.value;
     embedding.setModel(model);
     embedding.setDimension(parseInt(dimensionSlider.value));
     // calculate embeddings
+    setTimeout(() => {
+        submitModelBtn.innerHTML = originalHTML;
+        submitModelBtn.disabled = false;
+    }, 5000);
 });
 
 
